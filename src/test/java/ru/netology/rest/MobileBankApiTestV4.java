86b36dfedee4fa42a40503f45d0c8e85fd8e2276
Galina <gana88tit@gmail.com>
2023-11-22 10:29:00 +0300

    change accounts.schema.json

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -13,7 +13,7 @@ class MobileBankApiTestV4 {
       given()
           .baseUri("http://localhost:9999/api/v1")
       // Выполняемые действия
-      .when()
+      .when().log().all()
           .get("/demo/accounts")
       // Проверки
       .then()
